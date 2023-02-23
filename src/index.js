9211e9ccfc118538490a714c71a98e45120e2d1b
aseelflihan <aseelflihan@gmail.com>
2023-02-23 13:32:44 +0300

    complated assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newTask = document.createElement("li");
+    const taskInput = document.getElementById("new-task-description");
+    newTask.innerText = taskInput.value;
+    taskList.appendChild(newTask);
+    taskInput.value = "";
+  });
 });
